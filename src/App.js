658a4f6cb16d8bff05d3acd1df87ab96e2daa102
Jonathan Pope <jonathanpope@Jonathans-MacBook-Pro.local>
2019-08-07 11:34:50 -0400

    Added disclaimer to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import GridLayout from './components/layout/GridLayout'
 
 function App() {
-  return (
+   
+   alert('Disclaimer, while the information on this website is technically accurate this is NOT Mr. Kerkhoff’s official website. Any and all information is unofficial, non-binding and ineffectual.')
+   
+   return (
       <GridLayout />
-  )
+   )
 }
 
 export default App
